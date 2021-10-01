7bc073a14621ebbeaa7eb1894ca3dc1472e86552
Jithin Madhusudanan S <msjithin108@gmail.com>
2021-09-30 20:34:38 -0500

    working version of key usage

--- next_step.py
+++ next_step.py
@@ -9,15 +9,10 @@ option_url = 'https://api.tdameritrade.com/v1/marketdata/chains'
 
 symbols = ['AAPL', 'FB']
 
-
 def get_fundamentals(ticker=""):
     start, end = 0, 500
     
-<<<<<<< HEAD
     payload = {'apikey': st.secrets["ameritrade_key"] ,
-=======
-    payload = {'apikey':st.secrets["ameritrade_key"],
->>>>>>> f753c4de2693ae0c8dfe60c2406b4b35a51e9e2b
             'symbol':ticker,
             'projection':'fundamental'
             }
